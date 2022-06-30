8488f8f669579ad130c5701dfacd72721fbf78b5
ppresto <ppresto@hashicorp.com>
2022-06-30 13:36:08 -0700

    tgw attachment for vpc2

--- aws_ec2/network-vpc.tf
+++ aws_ec2/network-vpc.tf
@@ -34,3 +34,9 @@ module "vpc" {
     Name = "${var.prefix}-vpc2-public-rt"
   }
 }
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
+  subnet_ids         = [module.vpc.private_subnets]
+  transit_gateway_id = data.terraform_remote_state.aws_usw_dev_tgw.outputs.ec2_transit_gateway_id
+  vpc_id             = module.vpc.vpc_id
+}
