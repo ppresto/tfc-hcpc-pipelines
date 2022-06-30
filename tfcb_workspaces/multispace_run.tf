fd009edadcf969d4a2044fe91b7ee6f3acc5e11c
ppresto <ppresto@hashicorp.com>
2022-06-30 10:21:02 -0700

    2

--- tfcb_workspaces/multispace_run.tf
+++ tfcb_workspaces/multispace_run.tf
@@ -1,6 +1,4 @@
 resource "multispace_run" "hcp_consul" {
-  # Use string workspace names here and not data sources so that
-  # you can define the multispace runs before the workspace even exists.
   organization = var.organization
   workspace    = "aws_shared_hcp-consul"
 }
