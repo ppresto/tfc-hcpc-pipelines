10cc1882102132f68d624d07562f7874e2bd305d
ppresto <ppresto@hashicorp.com>
2022-06-30 15:31:13 -0700

    adding inner vpc route

--- aws_ec2/network-vpc.tf
+++ aws_ec2/network-vpc.tf
@@ -40,14 +40,21 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
   transit_gateway_id = data.terraform_remote_state.aws_usw_dev_tgw.outputs.ec2_transit_gateway_id
   vpc_id             = module.vpc.vpc_id
 }
-resource "aws_route" "private" {
+resource "aws_route" "privateToHcp" {
   for_each               = toset(module.vpc.private_route_table_ids)
   route_table_id         = each.key
   destination_cidr_block = data.terraform_remote_state.hcp_consul.outputs.hvn_cidr_block
   transit_gateway_id     = data.terraform_remote_state.aws_usw_dev_tgw.outputs.ec2_transit_gateway_id
 }
+
+resource "aws_route" "toAllVpc" {
+  for_each               = toset(module.vpc.private_route_table_ids)
+  route_table_id         = each.key
+  destination_cidr_block = "10.0.0.0/10"
+  transit_gateway_id     = data.terraform_remote_state.aws_usw_dev_tgw.outputs.ec2_transit_gateway_id
+}
 # VPC public subnet route to HCP CIDR Block
-resource "aws_route" "public" {
+resource "aws_route" "publicToHcp" {
   for_each               = toset(module.vpc.public_route_table_ids)
   route_table_id         = each.key
   destination_cidr_block = data.terraform_remote_state.hcp_consul.outputs.hvn_cidr_block
