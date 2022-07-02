5c31043ae3e99833e288b7a5e68d04b0ab6ffec9
ppresto <ppresto@hashicorp.com>
2022-07-01 17:37:41 -0700

    add notes and args

--- tfcb_workspaces/scripts/push-local-sshkeypair-to-aws.sh
+++ tfcb_workspaces/scripts/push-local-sshkeypair-to-aws.sh
@@ -1,32 +1,43 @@
 #!/usr/bin/env bash
 
-# Push local ssh key to 1 or every region in AWS using AWS CLI,
-# and set SSH_KEY_NAME variable used by the ./addAdmin_workspace.sh
-
-unique_num=$(shuf -i 0-1000 -n 1)
-
-# Requirement:  
+# Description:
+# Push local ssh key at $HOME/.ssh/id_rsa.pub to 1 or all regions in AWS using the AWS CLI
+# This script will also set the AWS_SSH_KEY_NAME variable used by ./addAdmin_workspace.sh
 #
-# Create or use an existing ssh key $HOME/.ssh/id_rsa.pub
-# Provide the aws_keypair_name as input to the script.
-# If nothing is provided the destination AWS keypair will be generated.
+# TIP
+# use source ./addAdmin_workspace.sh to update your current shell with AWS_SSH_KEY_NAME.
 
-if [[ -z "${1}" ]]; then
-  aws_keypair_name="my-aws-keypair-${unique_num}"
-else
-  aws_keypair_name="${1}"
-fi
-export SSH_KEY_NAME=${aws_keypair_name}
+# Requirement:
+# $HOME/.ssh/id_rsa.pub or create one with ssh-keygen.
+
+# Usage Examples:
+# The default with no args will create keypair name "my-aws-keypair-###" and copy it to all AWS regions
+# source ./push-local-sshkeypair-to-aws.sh
+#
+# Create keypair name supplied in arg1 and copy to all AWS regions.
+# source ./push-local-sshkeypair-to-aws.sh <my-keypair-name>
+#
+# Create keypair name supplied in arg1 and copy to region supplied in arg2.
+# source ./push-local-sshkeypair-to-aws.sh <my-keypair-name> <aws-region>
 
 # Path to PUBLIC ssh key that you want pushed to AWS
 publickeyfile="$HOME/.ssh/id_rsa.pub"
 
-# set target region or by default all available regions will be used.
-#regions="us-west-2"
+# use supplied keypair name or by default create unique keypair name
+if [[ -z "${1}" ]]; then
+  unique_num=$(shuf -i 0-1000 -n 1)
+  aws_keypair_name="my-aws-keypair-${unique_num}"
+else
+  aws_keypair_name="${1}"
+fi
+# source variable into the environment for ./addAdmin_workspace.sh
+export AWS_SSH_KEY_NAME=${aws_keypair_name}
 
-# If regions is not set get all regions
-if [[ -z $regions ]]; then
-regions=$(aws ec2 describe-regions \
+# set target region or by default all available regions will be used.
+if [[ ! -z "${2}" ]]; then
+  regions="${2}"
+else
+  regions=$(aws ec2 describe-regions \
   --output text \
   --query 'Regions[*].RegionName')
 fi
