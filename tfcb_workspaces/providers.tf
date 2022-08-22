20fdb55d763067fe87b51fb5cbd4e5606d52a450
ppresto <ppresto@hashicorp.com>
2022-08-22 15:57:16 -0700

    updated base code provider version

--- tfcb_workspaces/providers.tf
+++ tfcb_workspaces/providers.tf
@@ -7,11 +7,8 @@ terraform {
   required_version = ">= 1.0.5"
   required_providers {
     tfe = {
-      version = "~>0.31.0"
-    }
-    multispace = {
-      source  = "mitchellh/multispace"
-      version = "~>0.1.0"
+      source = "hashicorp/tfe"
+      version = "~>0.36.0"
     }
   }
 }
