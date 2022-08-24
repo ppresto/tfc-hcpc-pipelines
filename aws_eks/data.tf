b45a9a64899e2b6a61ba425c6eaede1ecf8f472d
ppresto <ppresto@hashicorp.com>
2022-08-24 16:15:06 -0700

    eks ds remove

--- aws_eks/data.tf
+++ aws_eks/data.tf
@@ -5,16 +5,6 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "terraform_remote_state" "aws_usw_dev_tgw" {
-  backend = "remote"
-  config = {
-    organization = var.organization
-    workspaces = {
-      name = "aws_${local.region_shortname}_${var.env}_network-tgw"
-    }
-  }
-}
-
 data "terraform_remote_state" "hcp_consul" {
   backend = "remote"
   config = {
