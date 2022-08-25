aa10fde43b697de4d91e9dd62305c6fd570c6239
ppresto <ppresto@hashicorp.com>
2022-08-25 16:32:19 -0700

    5

--- aws_eks/eks.tf
+++ aws_eks/eks.tf
@@ -86,24 +86,7 @@ module "eks" {
   }
 }
 
-resource "aws_security_group_rule" "consul_server_allow_client_8301" {
-  security_group_id        = module.eks.node_security_group_id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 8301
-  to_port                  = 8301
-  cidr_blocks              = ["10.0.0.0/10"]
-  description              = "Gossip between client agents"
-}
-resource "aws_security_group_rule" "consul_server_allow_client_8301_udp" {
-  security_group_id        = module.eks.node_security_group_id
-  type                     = "ingress"
-  protocol                 = "udp"
-  from_port                = 8301
-  to_port                  = 8301
-  cidr_blocks              = ["10.0.0.0/10"]
-  description              = "Used to handle gossip between client agents"
-}
+
 ################################################################################
 # Supporting Resources
 ################################################################################
