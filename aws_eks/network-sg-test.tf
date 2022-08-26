89c6b2bac89925831bdb5b52956d5e456562f959
ppresto <ppresto@hashicorp.com>
2022-08-25 17:20:22 -0700

    7

--- aws_eks/network-sg-test.tf
+++ aws_eks/network-sg-test.tf
@@ -16,3 +16,22 @@ resource "aws_security_group_rule" "consul_server_allow_client_egress_8301_udp" 
   source_security_group_id = aws_security_group.consul_server.id
   description              = "Used to handle gossip between client agents"
 }
+
+resource "aws_security_group_rule" "consul_server_allow_client_ingress_8301" {
+  security_group_id        = module.eks.cluster_primary_security_group_id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 8301
+  to_port                  = 8301
+  source_security_group_id = aws_security_group.consul_server.id
+  description              = "Used to handle gossip between client agents"
+}
+resource "aws_security_group_rule" "consul_server_allow_client_ingress_8301_udp" {
+  security_group_id        = module.eks.cluster_primary_security_group_id
+  type                     = "ingress"
+  protocol                 = "udp"
+  from_port                = 8301
+  to_port                  = 8301
+  source_security_group_id = aws_security_group.consul_server.id
+  description              = "Used to handle gossip between client agents"
+}
