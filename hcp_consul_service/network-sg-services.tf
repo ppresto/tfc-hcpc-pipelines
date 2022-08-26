87f7f0669bb1e8c16357bb91ba0cc11e096b6dc9
ppresto <ppresto@hashicorp.com>
2022-08-26 13:36:14 -0700

    restrict shared vpc sg

--- hcp_consul_service/network-sg-services.tf
+++ hcp_consul_service/network-sg-services.tf
@@ -12,30 +12,8 @@ resource "aws_security_group_rule" "service_envoy" {
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 20000
-  to_port           = 21255
-  cidr_blocks       = [var.vpc_cidr_block]
+  to_port           = 20000
+  cidr_blocks       = local.private_cidr_blocks
   ipv6_cidr_blocks  = length(var.allowed_bastion_cidr_blocks_ipv6) > 0 ? var.allowed_bastion_cidr_blocks_ipv6 : null
   description       = "Allow SSH traffic."
-}
-
-resource "aws_security_group_rule" "service_9090-9099" {
-  security_group_id = aws_security_group.service.id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 9090
-  to_port           = 9099
-  cidr_blocks       = [var.vpc_cidr_block]
-  ipv6_cidr_blocks  = length(var.allowed_bastion_cidr_blocks_ipv6) > 0 ? var.allowed_bastion_cidr_blocks_ipv6 : null
-  description       = "Allow SSH traffic."
-}
-
-resource "aws_security_group_rule" "service_allow_outbound" {
-  security_group_id = aws_security_group.service.id
-  type              = "egress"
-  protocol          = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = length(var.allowed_bastion_cidr_blocks_ipv6) > 0 ? ["::/0"] : null
-  description       = "Allow any outbound traffic."
 }
