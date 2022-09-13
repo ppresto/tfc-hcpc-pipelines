d5aa6b7a96cc4cfe2f42c6af00617cf5c1eba9f4
ppresto <ppresto@hashicorp.com>
2022-09-13 14:42:02 -0700

    test -open sg

--- aws_team2_eks/sg-hcp-consul.tf
+++ aws_team2_eks/sg-hcp-consul.tf
@@ -133,12 +133,22 @@ resource "aws_security_group_rule" "eks_envoy" {
   description       = "Allow envoy traffic."
 }
 
-resource "aws_security_group_rule" "hcp_tcp_https" {
+resource "aws_security_group_rule" "hcp_all_egress" {
   security_group_id = module.eks.cluster_primary_security_group_id
   type              = "egress"
-  protocol          = "tcp"
-  from_port         = 443
-  to_port           = 443
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
   cidr_blocks       = [local.hvn_cidr_block]
   description       = "The HTTPS API"
+}
+
+resource "aws_security_group_rule" "eks_all_ingress_test" {
+  security_group_id = module.eks.cluster_primary_security_group_id
+  type              = "ingress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = local.private_cidr_blocks
+  description       = "Allow envoy traffic."
 }
