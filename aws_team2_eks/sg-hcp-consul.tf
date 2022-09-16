fe026ec77a5f8ee45444ea14915792f294383328
ppresto <ppresto@hashicorp.com>
2022-09-16 10:10:28 -0700

    remove allow all to hcp to team2

--- aws_team2_eks/sg-hcp-consul.tf
+++ aws_team2_eks/sg-hcp-consul.tf
@@ -133,15 +133,15 @@ resource "aws_security_group_rule" "eks_envoy" {
   description       = "Allow envoy traffic."
 }
 
-resource "aws_security_group_rule" "hcp_all_egress" {
-  security_group_id = module.eks.cluster_primary_security_group_id
-  type              = "egress"
-  protocol          = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_blocks       = [local.hvn_cidr_block]
-  description       = "The HTTPS API"
-}
+#resource "aws_security_group_rule" "hcp_all_egress" {
+#  security_group_id = module.eks.cluster_primary_security_group_id
+#  type              = "egress"
+#  protocol          = "-1"
+#  from_port         = 0
+#  to_port           = 0
+#  cidr_blocks       = [local.hvn_cidr_block]
+#  description       = "The HTTPS API"
+#}
 resource "aws_security_group_rule" "hcp_all_egress2" {
   security_group_id = module.eks.cluster_primary_security_group_id
   type              = "egress"
@@ -160,12 +160,12 @@ resource "aws_security_group_rule" "eks_all_ingress_test" {
   cidr_blocks       = local.private_cidr_blocks
   description       = "Allow envoy traffic."
 }
-resource "aws_security_group_rule" "eks_all_ingress_test2" {
-  security_group_id = module.eks.cluster_primary_security_group_id
-  type              = "ingress"
-  protocol          = "-1"
-  from_port         = 0
-  to_port           = 0
-  cidr_blocks       = [local.hvn_cidr_block]
-  description       = "Allow envoy traffic."
-}
+#resource "aws_security_group_rule" "eks_all_ingress_test2" {
+#  security_group_id = module.eks.cluster_primary_security_group_id
+#  type              = "ingress"
+#  protocol          = "-1"
+#  from_port         = 0
+#  to_port           = 0
+#  cidr_blocks       = [local.hvn_cidr_block]
+#  description       = "Allow envoy traffic."
+#}
